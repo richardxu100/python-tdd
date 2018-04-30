63bbdf28914888761609af42ff91a7b83973acbd
Richard Xu <richard.xu@roadbotics.com>
2018-04-29 21:24:51 -0400

    Add more tests to functional_tests.py

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_page(request):
-    return render(request, 'home.html')
+# def home_page(request):
+#     return render(request, 'home.html', {
+#         'new_item_text': request.POST.get('item_text', ''), # supplies a default value, incase nothing provided
+#     })
     
+
+def home_page(request):
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
--- notes.md
+++ notes.md
@@ -4,5 +4,7 @@
 ## Notes
 * Functional tests test the application from the outside, from the perspective of the user. Unit tests test functionality from the inside out, from the perspective of the programmer
 
+**Progress:** Good, our deliberately silly return value is now no longer fooling our tests, so we are allowed to rew...
+https://www.obeythetestinggoat.com/book/chapter_post_and_database.html
 
 
